6d553a3c69e3d83c2461c26f3a7d4049b32810c5
Tatsuya Iwanaga <e165740@ie.u-ryukyu.ac.jp>
2017-02-05 17:30:03 +0900

    commit 1

---
+++ src/main/java/jp/ac/uryukyu/ie/e165740/main.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.e165740;
+
+import java.util.Scanner;
+
+/**
+ * Created by e165740 on 2017/01/31.
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+
+        System.out.print("ルーレット\n");
+        System.out.println("どれを行いますか？\n 1.ルーレット\n 2.ルール説明\n");
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("Input:");
+        int a = stdIn.nextInt();
+        
+    }
+}
