494ab99bfc95a89b7c40c3e1ed18bead2a0194e7
Tatsuya Iwanaga <e165740@ie.u-ryukyu.ac.jp>
2017-02-06 17:34:54 +0900

    commit 4

--- src/main/java/jp/ac/uryukyu/ie/e165740/Solver.java
+++ src/main/java/jp/ac/uryukyu/ie/e165740/Solver.java
@@ -1,44 +1,61 @@
 package jp.ac.uryukyu.ie.e165740;
 
 import java.util.Scanner;
-import java.util.concurrent.SynchronousQueue;
 
 /**
  * Created by e165740 on 2017/02/05.
  */
 public class Solver {
-
+    private int money = 100;
     int[] a = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+    int betMoney;
+    int betMunber;
+    int hitNum;
+    //現在の所持金、賭け金の設定、賭ける数字の設定を行う
+    public void bet(int money){
 
-    //現在の所持金、賭け金の設定、賭ける数字の設定を行うメソッド
-    public static void bet(){
 
-        int money = 100;
         // 所持金を示す
         System.out.printf("現在の所持金は $ %d です。\n",money);
         //掛け金を決める
         System.out.println("掛け金はいくらにしますか？");
         Scanner stdIn1 = new Scanner(System.in);
         System.out.print("bet is: $ ");
-        int betMoney = stdIn1.nextInt();
+        betMoney = stdIn1.nextInt();
         //賭ける数字を決める
         System.out.println("賭ける数字を入力してください。");
         Scanner stdIn2 = new Scanner(System.in);
         System.out.print("賭ける数字は:");
-        int betMunber = stdIn2.nextInt();
+        betMunber = stdIn2.nextInt();
 
     }
 
-    //当選番号を出力するメソッド
-    public static void turn(){
-        int hitNum = (int)(Math.random()*12);
+    //当選番号を出力する
+    public void turn(){
+        hitNum = (int)(Math.random()*12);
         System.out.printf("当選番号は「 %d 」\n",hitNum);
     }
 
-    //賭け金、賭けた数字によって配当を決め、所持金にプラスするメソッド
-    public static void profit(){
-        System.out.print("配当は $ %d です");
+    //賭け金、賭けた数字によって配当を決め、所持金にプラス,またはマイナスにする
+    public void profit(){
+        money -= betMoney;
+        if(betMunber == hitNum){
+            System.out.print("大当たり！！\n");
+            betMoney = betMoney*2;
+            money += betMoney;
+        }else{
+            System.out.print("ハズレ\n");
+            betMoney = 0;
+        }
+        System.out.printf("配当は $ %d です\n",betMoney);
+    }
+
+    public int getMoney(){
+        return money;
+    }
+
+    public void setMoney(int money){
+        this.money = money;
     }
 
 }
-
--- src/main/java/jp/ac/uryukyu/ie/e165740/main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165740/main.java
@@ -9,7 +9,7 @@ public class main {
 
     public static void main(String[] args) {
 
-
+        Solver solver = new Solver();
         System.out.print("ルーレット\n");
         System.out.println("どれを行いますか？\n 1.ルーレット\n 2.ルール説明\n");
         Scanner stdIn = new Scanner(System.in);
@@ -20,13 +20,20 @@ public class main {
             case 1:
                 System.out.println("");
 
-                Solver solver = new Solver();
-                Solver.bet();
-                Solver.turn();
+                while(solver.getMoney() > 0) {
+                    solver.bet(solver.getMoney());
+                    solver.turn();
+                    solver.profit();
+                }
+                System.out.println("終了します");
 
-                break;
             case 2:
                 System.out.println(" ------ルール説明------\n ");
+                System.out.println("ルーレットは1~12の中の数字が存在している。\n");
+                System.out.println("所持金は $ 100　で、まず賭け金を決めて、続いて賭ける数字を決める。\n");
+                System.out.println("数字が当たれば、賭け金の倍の金額を与えられる。\n");
+                System.out.println("所持金が 0 になるとゲームオーバー");
+
 
                 break;
             default:
